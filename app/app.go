db53e254625ef199c6fd01612b3aea58eac70a46
Pavan Kumar Sampara <23577468+pavan3999@users.noreply.github.com>
2025-04-03 07:08:11 +0530

    Update app.go

--- app/app.go
+++ app/app.go
@@ -4,8 +4,8 @@ package app
 //go:generate go run app_generate.go
 
 var (
-	LithiumAPK      = "Lithium_0.24.5.1.apk"
-	LithiumURL_APKM = "https://www.apkmirror.com/apk/faultexception/lithium-epub-reader/lithium-epub-reader-0-24-5-1-release/"
-	LithiumURL_IA   = "https://www.apkmirror.com/apk/faultexception/lithium-epub-reader/lithium-epub-reader-0-24-5-1-release/lithium-epub-reader-0-24-6-android-apk-download/download/?*"
-	LithiumSHA      = "4bb20673aa4a427bf2f12c2c7b14d87581d9a849826e53c0ef681f81d7d25e44"
+	LithiumAPK      = "Lithium_0.24.6.apk"
+	LithiumURL_APKM = "https://www.apkmirror.com/apk/faultexception/lithium-epub-reader/lithium-epub-reader-0-24-6-release/"
+	LithiumURL_IA   = "https://www.apkmirror.com/apk/faultexception/lithium-epub-reader/lithium-epub-reader-0-24-6-release/lithium-epub-reader-0-24-6-android-apk-download/download/?*"
+	LithiumSHA      = "2849845acce9f34f77f39e95347f96f867fc6d1e4e0ae3f9e0e22a6161f3bd3f"
 )
