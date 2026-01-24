3653ccfdae255807c8d600bfa62c384718fa18df
Pavan Kumar Sampara <23577468+pavan3999@users.noreply.github.com>
2026-01-24 13:41:22 +0530

    Update LithiumURL_IA to correct APK download link

--- app/app.go
+++ app/app.go
@@ -6,6 +6,6 @@ package app
 var (
 	LithiumAPK      = "Lithium_0.24.5.1.apk"
 	LithiumURL_APKM = "https://www.apkmirror.com/apk/faultexception/lithium-epub-reader/lithium-epub-reader-0-24-5-1-release/"
-	LithiumURL_IA   = "https://www.apkmirror.com/apk/faultexception/lithium-epub-reader/lithium-epub-reader-0-24-5-1-release/lithium-epub-reader-0-24-6-android-apk-download/download/?*"
+	LithiumURL_IA   = "https://www.apkmirror.com/apk/faultexception/lithium-epub-reader/lithium-epub-reader-0-24-5-1-release/lithium-epub-reader-0-24-5-1-android-apk-download/download/?*"
 	LithiumSHA      = "4bb20673aa4a427bf2f12c2c7b14d87581d9a849826e53c0ef681f81d7d25e44"
 )
